6b592a9a4e182fdeb3c8b25a77d2bda2c361b3e4
itsxavs <javitoo.lol@gmail.com>
2024-06-13 18:43:02 +0200

    aws

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useWebSocketAdapter(new IoAdapter(app));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
